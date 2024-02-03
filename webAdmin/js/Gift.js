50f6ac30182f25a5d1af8794693b409e417a5618
techyyhimanshu <158696334+techyyhimanshu@users.noreply.github.com>
2024-02-03 11:59:13 +0530

    Add files via upload

---
+++ webAdmin/js/Gift.js
@@ -0,0 +1,32 @@
+﻿function dropGift() {
+    console.log("running");
+    // Array of possible gift images
+    var giftImages = ["../Images/Present1.png", "../Images/Present2.png", "../Images/Present3.png", "../Images/Present4.png", "../Images/Present5.png", "../Images/Present6.gif"];
+
+    // Randomly select a gift image from the array
+    var randomGiftImage = giftImages[Math.floor(Math.random() * giftImages.length)];
+
+    // Create a new image element for the falling gift
+    var giftImage = document.createElement("img");
+    giftImage.src = randomGiftImage;
+    giftImage.alt = "Falling Gift";
+    giftImage.classList.add("falling-gift");
+
+    giftImage.style.width = "35px";
+    //giftImage.style.height = "35px";
+    giftImage.style.zIndex = "10000";
+
+    // Set the initial position below the clicked image
+    var padoruImage = document.getElementById("padoruImage");
+    var rect = padoruImage.getBoundingClientRect();
+    giftImage.style.left = rect.left + "px";
+    giftImage.style.top = rect.bottom + "px"; // Set top position below the clicked image
+
+    // Append the gift image to the body
+    document.body.appendChild(giftImage);
+
+    // Remove the gift image after the animation completes
+    giftImage.addEventListener("animationend", function () {
+        document.body.removeChild(giftImage);
+    });
+}
---
+++ webAdmin/js/snowfall.js
@@ -0,0 +1,36 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    setInterval(createSnowflake, 500); // Create a new snowflake every 1000 milliseconds (1 second)
+    //setInterval(createGift, 10000); // Create a new snowflake every 1000 milliseconds (1 second)
+});
+
+function createSnowflake() {
+    var snowflake = document.createElement('div');
+    snowflake.className = 'snowflake';
+    snowflake.style.left = Math.random() * 100 + 'vw';
+    snowflake.style.animationDuration = 5 + Math.random() * 10 + 's'; // Adjusted for more regular fall
+    snowflake.style.width = (Math.random() * 1 + 15) + 'px';
+    snowflake.style.height = (Math.random() * 1 + 15) + 'px';
+
+
+    document.body.appendChild(snowflake);
+
+    // Remove the snowflake after animation completes
+    snowflake.addEventListener('animationiteration', function () {
+        snowflake.remove();
+    });
+}
+
+function createGift() {
+    var snowflake = document.createElement('div');
+    snowflake.className = 'snowGift';
+    snowflake.style.left = Math.random() * 100 + 'vw';
+    snowflake.style.animationDuration = 5 + Math.random() * 10 + 's'; // Adjusted for more regular fall
+
+
+    document.body.appendChild(snowflake);
+
+    // Remove the snowflake after animation completes
+    snowflake.addEventListener('animationiteration', function () {
+        snowflake.remove();
+    });
+}
