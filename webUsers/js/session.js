3074c7a26dcfabe0f019d98e8ec6ef3d90f59644
techyyhimanshu <158696334+techyyhimanshu@users.noreply.github.com>
2024-02-03 12:11:07 +0530

    Add files via upload

---
+++ webUsers/js/session.js
@@ -0,0 +1,35 @@
+﻿// External JavaScript file
+
+function updateSessionCountdown() {
+    var timeoutSeconds = sessionTimeoutSeconds;
+
+    var intervalId = setInterval(function () {
+        var minutes = Math.floor(timeoutSeconds / 60);
+        var seconds = timeoutSeconds % 60;
+
+        var sessionCountdownElement = document.getElementById('<%= sessionCountdown.ClientID %>');
+
+        if (sessionCountdownElement) {
+            sessionCountdownElement.innerHTML = minutes + 'm ' + seconds + 's';
+        }
+
+        timeoutSeconds--;
+
+        if (timeoutSeconds < 0) {
+            // Display an error message
+            if (sessionCountdownElement) {
+                sessionCountdownElement.innerHTML = 'Session Expired';
+            }
+
+            // Redirect to the login page after a delay (e.g., 3 seconds)
+            setTimeout(function () {
+                window.location.href = 'Login.aspx'; // Redirect to your login page
+            }, 3000);
+
+            // Clear the interval to stop updating the timer
+            clearInterval(intervalId);
+        }
+    }, 1000);
+}
+
+window.onload = updateSessionCountdown;
